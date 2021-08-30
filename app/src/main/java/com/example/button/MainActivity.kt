e1041a1eafaf6e7656b3a2f7cb5d85754970447f
Agustin Falcon <agustinrobertofalcon@gmail.com>
2021-08-30 11:56:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/button/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.button.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
+
+    }
+}
